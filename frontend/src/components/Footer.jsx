3ed136f8f9b9ea6c9b96dd1298b9a9fecc0ab61b
Victoria Beasley <iesnoth91@gmail.com>
2023-01-09 20:17:20 -0600

    john's merge

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Footer = () => {
     return(
-        <div>
-            <h3>This is the footer.</h3>
+        <div className='main-footer'>
+            <div className= 'container'>
+                <h3>This is the footer.</h3>
+            </div>
+            <br/>
+            <p className='copyright'>
+                &copy; {new Date().getFullYear()} | Squatch Watch Inc. | All rights reserved 
+            </p>
         </div>
     )
 }
