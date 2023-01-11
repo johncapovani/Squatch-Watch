bf48471ec79357930cdd57f1b7352a201292f9a3
Victoria Beasley <iesnoth91@gmail.com>
2023-01-10 18:34:34 -0600

    rough draft of datacard

--- frontend/src/components/Datacard.jsx
+++ frontend/src/components/Datacard.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
-const Datacard = () => {
+const Datacard = (props) => {
     return(
-        <div>
-            <p>
-                Datacard
-            </p>
+        <div className='whole-card'>
+            <div className='col-one'>
+                <img src={props.item.images} alt={props.item.species}/>
+                <h3>Species Spotted: {props.item.species}</h3>
+                <h3>Spotted by: {props.item.user}</h3>
+            </div>
+            <div className='col-two'>
+                <h2>{props.item.date}</h2>
+                <h3>{props.item.time}</h3>
+                <h3>Location: {props.item.location}</h3>
+                <div className='description '>
+                    <h4>Report Account</h4>
+                    <p>{props.item.description}</p>
+                </div>
+            </div>
         </div>
     )
 }
