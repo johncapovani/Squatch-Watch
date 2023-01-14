5832adcda743b79054b916ac4ee9462cd325e2c3
Victoria Beasley <iesnoth91@gmail.com>
2023-01-14 15:38:27 -0600

    making sure everything's saved before signing off

--- frontend/src/components/Datacard.jsx
+++ frontend/src/components/Datacard.jsx
@@ -5,7 +5,18 @@ const Datacard = ({sighting}) => {
     return(
         <>
         <div className="goal">
-            <h2>{sighting.time}</h2>
+            <div className="column-one">
+                <img/>
+                <div>
+                    <h4> Reported by: {sighting.user}</h4>
+                    <h4> Species: {sighting.species} </h4>
+                </div>
+            </div>
+            <div className="column-two">
+                <h2>{sighting.date}</h2>
+                <h4>{sighting.time}</h4>
+                <p className="description">{sighting.description}</p>
+            </div>
         </div>
         </>
     )
