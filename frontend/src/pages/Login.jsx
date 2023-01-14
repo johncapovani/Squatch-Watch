976f89c6d1f6839a93359be91fc19f14d9351d4e
John Capovani <john@capovani.com>
2023-01-14 14:10:19 -0500

    Updated css, register, logout functionality.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import { login, reset } from '../features/auth/authSlice'
 import Spinner from '../components/Spinner'
+import "./Login.css"
 
 function Login() {
   const [formData, setFormData] = useState({
@@ -57,47 +58,47 @@ function Login() {
 
   return (
     <>
-    <div className='login'>
-      <section className='heading'>
-        <h1>
-          <FaSignInAlt /> Login
-        </h1>
-        <p>Login to interact with Squatch Watch!</p>
-      </section>
+      <div className='login'>
+        <section className='heading'>
+          <h1>
+            <FaSignInAlt /> Login
+          </h1>
+          <p>Login to interact with Squatch Watch!</p>
+        </section>
 
-      <section>
-        <form className='lForm' onSubmit={onSubmit}>
-          <div className='form-group'>
-            <input
-              type='email'
-              className='form-control'
-              id='email'
-              name='email'
-              value={email}
-              placeholder='Enter your email'
-              onChange={onChange}
+        <section>
+          <form className='lForm' onSubmit={onSubmit}>
+            <div className='form-group'>
+              <input
+                type='email'
+                className='form-control'
+                id='email'
+                name='email'
+                value={email}
+                placeholder='Enter your email'
+                onChange={onChange}
               />
-          </div>
-          <div className='form-group'>
-            <input
-              type='password'
-              className='form-control'
-              id='password'
-              name='password'
-              value={password}
-              placeholder='Enter password'
-              onChange={onChange}
+            </div>
+            <div className='form-group'>
+              <input
+                type='password'
+                className='form-control'
+                id='password'
+                name='password'
+                value={password}
+                placeholder='Enter password'
+                onChange={onChange}
               />
-          </div>
+            </div>
 
-          <div className='form-group'>
-            <button type='submit' className='btn btn-block'>
-              Submit
-            </button>
-          </div>
-        </form>
-      </section>
-    </div>
+            <div className='form-group'>
+              <button type='submit' className='btn btn-block'>
+                Submit
+              </button>
+            </div>
+          </form>
+        </section>
+      </div>
     </>
   )
 }
