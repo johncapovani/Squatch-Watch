509de1ddee93f93270514c3769b842997ba25a86
John Capovani <john@capovani.com>
2023-01-14 13:53:34 -0500

    Register and Login functionality is working. Created new protected routes.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa'
+
+//Access the state to determine if user is logged in or not
+import { useSelector, useDispatch } from 'react-redux'
+import { logout, reset } from '../features/auth/authSlice'
+
 
 
 function Navbar() {
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const { user } = useSelector((state) => state.auth)
+
+  //Logout a user
+
+  const onLogout = () => {
+    dispatch(logout())
+    dispatch(reset())
+    navigate('/')
+  }
+
   return (
     <header className='header'>
       <div>
@@ -15,23 +33,40 @@ function Navbar() {
             Report a Sighting
           </Link>
         </li>
-        <li>
-          <Link className='nav n2' to='/login'>
-            Login
-            <FaSignInAlt />
-          </Link>
-        </li>
-        <li>
-          <Link className='nav n3' to='/register'>
-            Register
-            <FaUser />
-          </Link>
-        </li>
-        <li>
-          <Link className='nav n4' to='/account'>
-            My Account
-          </Link>
-        </li>
+
+        {user ? (
+          <>
+
+            <li>
+              <Link className='nav n3' to='/account'>
+                My Account
+              </Link>
+            </li>
+
+            <li>
+              <button className='nav b1' onClick={onLogout}>
+                Logout
+                <FaSignOutAlt />
+              </button>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <Link className='nav n2' to='/login'>
+                Login
+                <FaSignInAlt />
+              </Link>
+            </li>
+            <li>
+              <Link className='nav n3' to='/register'>
+                Register
+                <FaUser />
+              </Link>
+            </li>
+          </>
+        )}
+
 
       </ul>
     </header>
