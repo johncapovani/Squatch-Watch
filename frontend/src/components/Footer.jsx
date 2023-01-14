c8ea5abb07411420ea68b708b789505d4a4781bf
Victoria Beasley <iesnoth91@gmail.com>
2023-01-14 01:14:22 -0600

    foot finished?

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './Footer.css'
+import {
+    faTwitter,
+    faFacebook,
+    faInstagram
+} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Footer = () => {
     return (
-        <>
+        <div className='footer'>
         <div className="footer-text">
+            <p className='icons'>
+                <b>Contact Us</b> <FontAwesomeIcon icon={faInstagram}/>     <FontAwesomeIcon icon={faTwitter}/>   <FontAwesomeIcon icon={faFacebook}/>
+            </p>
             <p className='copyright'>
                 &copy; {new Date().getFullYear()} | Squatch Watch Inc. | All rights reserved
             </p>
@@ -16,7 +25,7 @@ const Footer = () => {
 
             
         </div>
-        </>
+        </div>
     )
 }
 
